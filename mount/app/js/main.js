36ab8edeef666f5cea106ea6644af5a072f93e4b
mihail_morgunets <miha123770214@gmail.com>
2020-04-25 16:53:21 +0300

    add animate in project mount

--- mount/app/js/main.js
+++ mount/app/js/main.js
@@ -1,6 +1,20 @@
-$(function(){
+$(function () {
 
+  $('.home__content-btn').on('click', function () {
+    $('.service').get(0).scrollIntoView({
+      block: "start",
+      behavior: "smooth"
+    });
+  });
 
+  function scrollToElement(element, parent) {
+    $(parent)[0].scrollIntoView(true);
+    $(parent).animate({
+      scrollTop: $(parent).scrollTop() + $(element).offset().top - $(parent).offset().top
+    }, {
+      duration: 'slow',
+      easing: 'swing'
+    });
+  }
 
-  
 });
