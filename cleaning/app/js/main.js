741d11b8c0a6a1fac04067ee0672166b8643d28d
mihail_morgunets <miha123770214@gmail.com>
2020-05-05 14:58:31 +0300

    added project 'cleaning'

---
+++ cleaning/app/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+
+  $(window).scroll(function (event) {
+    let scroll = $(this).scrollTop();
+    let t_2 = scroll * 0.3;
+    let t_1 = scroll * 0.5;
+    $('.home__cubes-item--2').css('bottom', t_2 + 140 + 'px');
+    $('.home__cubes-item--1').css('top', (-1 * t_1) - 60 + 'px');
+  })
+
+});
