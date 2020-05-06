7e52761f0fd71d73ccfcc21c7e15c86963372cf8
mihail_morgunets <miha123770214@gmail.com>
2020-05-06 17:17:55 +0300

    add two section

--- cleaning/app/js/main.js
+++ cleaning/app/js/main.js
@@ -2,10 +2,19 @@ $(function () {
 
   $(window).scroll(function (event) {
     let scroll = $(this).scrollTop();
-    let t_2 = scroll * 0.45;
-    let t_1 = scroll * 0.75;
-    $('.home__cubes-item--2').css('bottom', t_2 + 140 + 'px');
-    $('.home__cubes-item--1').css('top', (-1 * t_1) - 60 + 'px');
+    let t_1 = scroll * 0.8;
+    let t_2 = scroll * 0.5;
+    let t_3 = scroll * 0.3;
+    let t_4 = scroll * 0.6;
+
+    $('.cubes-item--1').css('top', (-1 * t_1) - 60 + 'px');
+    $('.cubes-item--2').css('bottom', t_2 + 140 + 'px');
+    $('.cubes-item--3').css('top', (-1 * t_3) - 150 + 'px');
+    $('.cubes-item--4').css('top', (-1 * t_3) + 150 + 'px');
+  })
+
+  $('.services__item-img').on('click', function () {
+    $(this).toggleClass('active');
   })
 
 });
