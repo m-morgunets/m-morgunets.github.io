36141082c52f5df48698ce881f1f4d7cd99ac416
mihail_morgunets <miha123770214@gmail.com>
2020-05-07 18:41:27 +0300

    fix bug in slider

--- cleaning/app/js/main.js
+++ cleaning/app/js/main.js
@@ -34,23 +34,6 @@ $(function () {
     });
   }
 
-
-  // setInterval(() => {
-  //   if (n > 1) {
-  //     $('.slider__item:nth-child(' + (n - 1) + ')').css('opacity', 1)
-  //   }
-  //   if (n > 4) {
-  //     n = 1
-  //   }
-  //   $('.slider__item:nth-child(' + n + ')').css('opacity', 0)
-  //   n++
-  // }, 1000)
-
-  function mouseOver() {
-    $(this).css('opacity', 0);
-    $('.ui-slider-handle').css('z-index', 1);
-  }
-
   let n = 1;
   var steps = [1, 2, 3, 4];
 
@@ -60,69 +43,51 @@ $(function () {
     max: steps.length - 1,
     step: 1,
     slide: function (event, ui) {
-      $('.slider__runner-title:nth-child(' + n + ')').off('mouseover', mouseOver);
-      $('.slider__runner-title:nth-child(' + n + ')').css('color', '#4e4e4e');
+      $('.slider__item:nth-child(' + n + ')').css({
+        'opacity': 0,
+        'visibility': 'hidden'
+      });
+      $('.slider__runner-title:nth-child(' + n + ')').css({
+        'color': '#4e4e4e',
+        'opacity': 1
+      });
+      $('.ui-slider-handle ul li:nth-child(' + n + ')').css('opacity', 0);
+
+      $('.ui-slider-handle ul li:nth-child(' + steps[ui.value] + ')').css('opacity', 1);
       $('.slider__item:nth-child(' + steps[ui.value] + ')').css({
         'opacity': 1,
         'visibility': 'visible'
       });
-      $('.slider__runner-title:nth-child(' + steps[ui.value] + ')').css('color', '#e6e6e6');
-      $('.slider__runner-title:nth-child(' + steps[ui.value] + ')').on('mouseover', mouseOver);
+      $('.slider__runner-title:nth-child(' + steps[ui.value] + ')').css({
+        'color': '#e6e6e6',
+        'opacity': 0
+      });
       n = steps[ui.value];
     }
   });
+
+  let addUl = $('<ul><li>Кухня</li><li>Комната</li><li>Прихожая</li><li>Туалет</li></ul>')
+  $('.ui-slider-handle').html(addUl);
+
+  $('.ui-slider-handle ul').on('mousedown', () => {
+    $('.ui-slider-handle ul').css({
+      'cursor': 'grabbing',
+      'cursor': '-webkit-grabbing',
+      'opacity': 0
+    })
+  })
+  $('.ui-slider-handle ul').on('mouseup', () => {
+    $('.ui-slider-handle ul').css({
+      'cursor': 'grab',
+      'cursor': '-webkit-grab',
+      'opacity': 1
+    })
+  })
 
   $('.slider__item:nth-child(1)').css({
     'opacity': 1,
     'visibility': 'visible'
   })
-  $('.slider__runner-title:nth-child(1)').css('color', '#e6e6e6')
-  $('.slider__runner-title:nth-child(1)').on('mouseover', mouseOver);
-
-  // $('.slider__runner-title').on('mouseover', function () {
-  //   $(this).css('opacity', 0);
-  //   $('.ui-slider-handle').css('z-index', 1);
-  // });
-
-  $('.ui-slider-handle').on('mouseover', function () {
-    $(this).css('z-index', 1);
-  });
-  $('.ui-slider-handle').on('mouseout', function () {
-    $(this).css('z-index', 0);
-    setTimeout(() => {
-      $('.slider__runner-title').css('opacity', 1);
-    }, 200)
-
-  });
-
-  // $('.slider__runner-titles').on('mouseover', function () {
-  //   $(this).css('z-index', 1);
-  // });
-  // $('.slider__runner-titles').on('mouseout', function () {
-  //   $(this).css('z-index', 2);
-  // });
-
-  // let item = document.querySelectorAll('.services__item');
-  // let itemImg = document.querySelectorAll('.services__item-img');
-  // let content = document.querySelectorAll('.services__item-content');
-
-  // for (let i = 0; i <= item.length; i++) {
-  //   itemImg[i].addEventListener('mouseover', function () {
-  //     content[i].style.display = 'block';
-  //   });
-  //   item[i].addEventListener('mouseout', () => {
-  //     let timerId = setTimeout(function () {
-  //       content[i].style.display = 'none';
-  //     }, 1000);
-  //     itemImg[i].addEventListener('mouseover', function () {
-  //       content[i].style.display = 'block';
-  //       clearTimeout(timerId);
-  //     });
-  //     content[i].addEventListener('mouseover', function () {
-  //       this.style.display = 'block';
-  //       clearTimeout(timerId);
-  //     });
-  //   });
-  // };
-
+  $('.slider__runner-title:nth-child(1)').css('color', '#e6e6e6');
+  $('.ui-slider-handle ul li:nth-child(1)').css('opacity', 1);
 });
