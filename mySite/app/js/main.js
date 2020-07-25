9287368ec421909a9db440d14496bd80d3e210cb
mihail_morgunets <miha123770214@gmail.com>
2020-07-25 17:00:34 +0300

    add new possibility in my project

--- mySite/app/js/main.js
+++ mySite/app/js/main.js
@@ -1,5 +1,18 @@
 $(function () {
 
+  let topicNow = localStorage.getItem('topic');
+  $(`body`).addClass(topicNow);
+
+  $('.topic-btn').click(function (event) {
+
+    $('body').toggleClass('black');
+    localStorage.setItem('topic', 'black')
+
+    if (!$('body').is('.black')) {
+      localStorage.setItem('topic', 'white');
+    }
+  });
+
   $('img.svg').each(function () {
     var $img = $(this);
     var imgClass = $img.attr('class');
@@ -37,24 +50,91 @@ $(function () {
     });
   }
 
-  $('.topic-btn').on('click', function () {
-    $('body').toggleClass('black');
-  });
-
   const offsetSckills = $('.skills').offset().top;
-  scrollAnim();
+  scrollSkill();
+  scrillContacts();
 
-  $(window).scroll(function () {
-    scrollAnim()
+  $(window).on('scroll', function () {
+    scrollSkill();
+  });
+  $(window).on('scroll', function () {
+    scrillContacts();
   });
 
-  function scrollAnim() {
+  function scrollSkill() {
     if (offsetSckills - $(window).scrollTop() <= 100) {
-      $('.bg').addClass('animate')
-    }
-    if ($(window).scrollTop() >= 2400) {
-      $('.contacts').addClass('animate');
+      let skillsKey = 100;
+      for (const iterator of $('.bg')) {
+        setTimeout(() => {
+          $(iterator).addClass('animate');
+        }, skillsKey);
+        skillsKey += 100;
+      }
+      $(window).off('scroll', function () {
+        scrollSkill();
+      });
     }
   }
+
+  function scrillContacts() {
+    if ($(window).scrollTop() >= 2400) {
+      $('.contacts').addClass('animate');
+      $(window).off('scroll', function () {
+        scrillContacts();
+      });
+    }
+  }
+
+  function idGenerator(selector) {
+    let idCase = 0;
+    for (const iterator of $(selector)) {
+      idCase++
+      $(iterator).attr('id', idCase);
+    }
+  }
+  idGenerator('.portfolio__item');
+  idGenerator('.cases__item');
+
+  // $('.portfolio__item').on('click', function () {
+  //   let id = $(this).attr('id');
+  //   $('.cases').fadeIn();
+  //   $(`.cases__item[id='${id}']`).fadeIn();
+  // })
+
+  // $('.cases::before').on('click', function () {
+  //   $('.cases__item').fadeOut();
+  //   $('.cases').fadeOut();
+  // })
+
+
+
+  $('.portfolio__item').click(function () {
+    let id = $(this).attr('id');
+    $('.cases').fadeIn();
+    $(`.cases__item[id='${id}']`).fadeIn();
+    return false;
+  });
+
+  $('.cases__item-close').click(function () {
+    $(this).parents('.cases').fadeOut();
+    $('.cases__item').fadeOut();
+    return false;
+  });
+
+  $(document).keydown(function (e) {
+    if (e.keyCode === 27) {
+      e.stopPropagation();
+      $('.cases').fadeOut();
+      $('.cases__item').fadeOut();
+    }
+  });
+
+  $('.cases').click(function (e) {
+    if ($(e.target).closest('.cases__item').length == 0) {
+      $(this).fadeOut();
+      $('.cases__item').fadeOut();
+    }
+  });
+
 
 });
