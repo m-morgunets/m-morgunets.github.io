a3f3b5b2770237cea562db1bfe21740ac2d05016
mihail_morgunets <miha123770214@gmail.com>
2020-10-01 17:05:19 +0300

    add in project master-class

--- master-class/gulpfile.js
+++ master-class/gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('scss', function () {
 gulp.task('css', function () {
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
-    'node_modules/slick-carousel/slick/slick.css',
+    'node_modules/animate.css/animate.min.css',
   ])
     .pipe(concat('_libs.scss'))
     .pipe(gulp.dest('app/scss'))
@@ -45,7 +45,8 @@ gulp.task('script', function () {
 
 gulp.task('js', function () {
   return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js'
+    'node_modules/wow.js/dist/wow.js',
+    'node_modules/typed.js/lib/typed.min.js'
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
