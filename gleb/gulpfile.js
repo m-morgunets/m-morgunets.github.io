b95782e5f38a35650754520c14471ec04c3ad0a8
mihail_morgunets <miha123770214@gmail.com>
2020-12-07 16:15:11 +0300

    revisions in home page

--- gleb/gulpfile.js
+++ gleb/gulpfile.js
@@ -13,13 +13,13 @@ gulp.task('clean', async function () {
 })
 
 gulp.task('scss', function () {
-  return gulp.src('test/scss/**/*.scss')
+  return gulp.src('home/scss/**/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 8 versions']
     }))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('test/css'))
+    .pipe(gulp.dest('home/css'))
     .pipe(browserSync.reload({ stream: true }))
 });
 
@@ -27,64 +27,64 @@ gulp.task('css', function () {
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
-    'node_modules/animate.css/animate.css',
+    'animate.css',
   ])
     .pipe(concat('_libs.scss'))
-    .pipe(gulp.dest('test/scss'))
+    .pipe(gulp.dest('home/scss'))
     .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('html', function () {
-  return gulp.src('test/*.html')
+  return gulp.src('home/*.html')
     .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('script', function () {
-  return gulp.src('test/js/*.js')
+  return gulp.src('home/js/*.js')
     .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('js', function () {
   return gulp.src([
     'node_modules/slick-carousel/slick/slick.js',
+    'wow.js/dist/wow.min.js',
     'node_modules/three/build/three.min.js',
-    'node_modules/wow.js/dist/wow.js',
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('test/js'))
+    .pipe(gulp.dest('home/js'))
     .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('browser-sync', function () {
   browserSync.init({
     server: {
-      baseDir: "test/"
+      baseDir: "home/"
     }
   });
 });
 
 gulp.task('export', function () {
-  let buildHtml = gulp.src('test/**/*.html')
+  let buildHtml = gulp.src('home/**/*.html')
     .pipe(gulp.dest('dist'));
 
-  let BuildCss = gulp.src('test/css/**/*.css')
+  let BuildCss = gulp.src('home/css/**/*.css')
     .pipe(gulp.dest('dist/css'));
 
-  let BuildJs = gulp.src('test/js/**/*.js')
+  let BuildJs = gulp.src('home/js/**/*.js')
     .pipe(gulp.dest('dist/js'));
 
-  let BuildFonts = gulp.src('test/fonts/**/*.*')
+  let BuildFonts = gulp.src('home/fonts/**/*.*')
     .pipe(gulp.dest('dist/fonts'));
 
-  let BuildImg = gulp.src('test/img/**/*.*')
+  let BuildImg = gulp.src('home/img/**/*.*')
     .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('test/scss/**/*.scss', gulp.parallel('scss'));
-  gulp.watch('test/*.html', gulp.parallel('html'))
-  gulp.watch('test/js/*.js', gulp.parallel('script'))
+  gulp.watch('home/scss/**/*.scss', gulp.parallel('scss'));
+  gulp.watch('home/*.html', gulp.parallel('html'))
+  gulp.watch('home/js/*.js', gulp.parallel('script'))
 });
 
 gulp.task('build', gulp.series('clean', 'export'))
--- gleb/home/js/main.js
+++ gleb/home/js/main.js
@@ -1,13 +1,51 @@
 $(function () {
 
-    $(window).on('scroll', function () {
+    var wow = new WOW();
+    wow.init();
 
-        if ($(this).scrollTop() + $(window).height() * 0.25 >= $('.why').offset().top) {
-            lineAnimate();
+    let heigthIndex = 501;
+    let offset = 500;
+    let height = $('.why__box-line').height();
+    scroll();
+    function scroll() {
+        // if (scroll + $(window).height() * 0.25 >= $('.why').offset().top) {
+        //     lineAnimate();
+        // }
+
+        let scroll = $(this).scrollTop();
+        let scrollInSection = scroll + $(this).height() / 3 - $('.why__box').offset().top;
+
+        if (scrollInSection >= 0) {
+            $('.why__box-dot').fadeIn();
+            offset = (height - scrollInSection) / height * 500;
+            if (heigthIndex > offset) {
+                $('.line-animation').css('stroke-dashoffset', offset + '%');
+                heigthIndex = offset;
+            }
+            if (scrollInSection >= 115) {
+                $('.why__box-item.item-1').addClass('active');
+            }
+            if (scrollInSection >= 260) {
+                $('.why__box-item.item-2').addClass('active');
+            }
+            if (scrollInSection >= 380) {
+                $('.why__box-item.item-3').addClass('active');
+            }
+            if (scrollInSection >= 510) {
+                $('.why__box-item.item-4').addClass('active');
+            }
+            if (scrollInSection >= 650) {
+                $('.why__box-item.item-5').addClass('active');
+            }
         }
+        if (offset <= 0) {
+            $('.line-animation').css('stroke-dashoffset', '0');
+            $('.why__box-cup').addClass('active');
+            $(window).off();
+        }
+    }
 
-    })
-
+    $(window).on('scroll', scroll);
     function lineAnimate() {
         $('.why__box-dot').fadeIn();
         setTimeout(() => {
@@ -34,73 +72,65 @@ $(function () {
     }
 
 
-    // bgAnimate();
-    // setTimeout(() => {
-    //     $('.main__bg').fadeIn();
-    // }, 500);
+    // function bgAnimate() {
+    //     var leftIndex = 0;
+    //     var widthTrack = $('.main__bg-track').width();
+    //     for (var i = 1; i <= 2; i++) {
+    //         newBgBox();
+    //     }
+    //     var transformTrack = 0;
+    //     var trackIndicator = 0;
+    //     var indexItem = 1;
+    //     setInterval(() => {
+    //         $('.main__bg-track').css('transform', 'translateX(' + transformTrack + 'px)');
+    //         transformTrack -= 15;
+    //         trackIndicator += 15;
+    //         if (-(transformTrack / indexItem) >= widthTrack) {
+    //             indexItem++;
+    //             $('.main__bg-box:first-child').detach();
+    //             trackIndicator = 0;
+    //             newBgBox();
+    //         }
+    //     }, 100);
+    //     function newBgBox() {
+    //         $('.main__bg .main__bg-track').append('<div class="main__bg-box"></div>');
+    //         for (var j = 0; j < 30; j++) {
+    //             $('.main__bg .main__bg-box:last-child').append('<div class="main__bg-item"></div>');
+    //         }
+    //         $('.main__bg .main__bg-box:last-child').css('transform', 'translateX(' + (widthTrack * leftIndex) + 'px)');
+    //         leftIndex += 1;
+    //         for (var iterator of $('.main__bg-box:last-child .main__bg-item')) {
+    //             randomStyles(iterator);
+    //         }
+    //     }
 
-
-    function bgAnimate() {
-        var leftIndex = 0;
-        var widthTrack = $('.main__bg-track').width();
-        for (var i = 1; i <= 2; i++) {
-            newBgBox();
-        }
-        var transformTrack = 0;
-        var trackIndicator = 0;
-        var indexItem = 1;
-        setInterval(() => {
-            $('.main__bg-track').css('transform', 'translateX(' + transformTrack + 'px)');
-            transformTrack -= 15;
-            trackIndicator += 15;
-            if (-(transformTrack / indexItem) >= widthTrack) {
-                indexItem++;
-                $('.main__bg-box:first-child').detach();
-                trackIndicator = 0;
-                newBgBox();
-            }
-        }, 100);
-        function newBgBox() {
-            $('.main__bg .main__bg-track').append('<div class="main__bg-box"></div>');
-            for (var j = 0; j < 30; j++) {
-                $('.main__bg .main__bg-box:last-child').append('<div class="main__bg-item"></div>');
-            }
-            $('.main__bg .main__bg-box:last-child').css('transform', 'translateX(' + (widthTrack * leftIndex) + 'px)');
-            leftIndex += 1;
-            for (var iterator of $('.main__bg-box:last-child .main__bg-item')) {
-                randomStyles(iterator);
-            }
-        }
-
-        function randomStyles(object) {
-            var arrColor = ['rgba(255, 0, 0, ', 'rgba(255, 170, 0, ', 'rgba(0, 255, 170, ', 'rgba(153, 0, 255, ']
-            var positionX = randomInteger(0, 1) * $('.main__bg').width();
-            var positionY = randomInteger(0, 1) * $('.main__bg').height();
-            var opacity = randomInteger(0.1, 0.6);
-            var blur = randomInteger(1, 10);
-            var colorIndex = Math.round(randomInteger(0, 3));
-            var animate = Math.round(randomInteger(0, 1));
-            var timeOut = Math.round(randomInteger(1, 1000));
-            if (animate) {
-                setTimeout(() => {
-                    $(object).addClass('animate');
-                }, timeOut);
-            }
-            $(object).css({
-                'left': positionX,
-                'top': positionY,
-                'filter': 'blur(' + blur + 'px)',
-                'background': arrColor[colorIndex] + opacity + ')',
-            })
-        }
-    }
-
-
-    function randomInteger(min, max) {
-        // случайное число от min до (max)
-        let rand = min + Math.random() * (max - min);
-        return rand;
-    }
+    //     function randomStyles(object) {
+    //         var arrColor = ['rgba(255, 0, 0, ', 'rgba(255, 170, 0, ', 'rgba(0, 255, 170, ', 'rgba(153, 0, 255, ']
+    //         var positionX = randomInteger(0, 1) * $('.main__bg').width();
+    //         var positionY = randomInteger(0, 1) * $('.main__bg').height();
+    //         var opacity = randomInteger(0.1, 0.6);
+    //         var blur = randomInteger(1, 10);
+    //         var colorIndex = Math.round(randomInteger(0, 3));
+    //         var animate = Math.round(randomInteger(0, 1));
+    //         var timeOut = Math.round(randomInteger(1, 1000));
+    //         if (animate) {
+    //             setTimeout(() => {
+    //                 $(object).addClass('animate');
+    //             }, timeOut);
+    //         }
+    //         $(object).css({
+    //             'left': positionX,
+    //             'top': positionY,
+    //             'filter': 'blur(' + blur + 'px)',
+    //             'background': arrColor[colorIndex] + opacity + ')',
+    //         })
+    //     }
+    // }
+    // function randomInteger(min, max) {
+    //     // случайное число от min до (max)
+    //     let rand = min + Math.random() * (max - min);
+    //     return rand;
+    // }
 
     jQuery('img.svg').each(function () {
         var $img = jQuery(this);
